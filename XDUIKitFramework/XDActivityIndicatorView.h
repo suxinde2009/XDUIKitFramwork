7ecb928416e79825c769615791302ce80ae5439d
sxd <suxinde2009@126.com>
2013-09-27 16:46:33 +0800

    some modifications

---
+++ XDUIKitFramework/XDActivityIndicatorView.h
@@ -0,0 +1,13 @@
+//
+//  XDActivityIndicatorView.h
+//  XDUIKitFramework
+//
+//  Created by su xinde on 13-9-27.
+//  Copyright (c) 2013年 __MyCompany__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XDActivityIndicatorView : NSObject
+
+@end
---
+++ XDUIKitFramework/XDActivityIndicatorView.mm
@@ -0,0 +1,13 @@
+//
+//  XDActivityIndicatorView.m
+//  XDUIKitFramework
+//
+//  Created by su xinde on 13-9-27.
+//  Copyright (c) 2013年 __MyCompany__. All rights reserved.
+//
+
+#import "XDActivityIndicatorView.h"
+
+@implementation XDActivityIndicatorView
+
+@end
